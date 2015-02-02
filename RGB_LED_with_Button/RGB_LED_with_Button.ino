f098ee083e1da67c1c091512a3365024deebdd15
logankd <logk34@hotmail.com>
2015-02-02 09:12:14 -0600

    updated RGB_LED

--- RGB_LED_with_Button/RGB_LED_with_Button.ino
+++ RGB_LED_with_Button/RGB_LED_with_Button.ino
@@ -11,44 +11,80 @@ void setup()
    pinMode(greenPin, OUTPUT); // sets the greenPin to be an output 
    pinMode(bluePin, OUTPUT); // sets the bluePin to be an output 
 }    
+
+int color = 0;
 /***************************************************************************/      
 void loop()  // run over and over again  
 {    
   if(digitalRead(buttonPin) == HIGH )
   {
+    changeColor();
+    delay(1000);
+    /*
           // Basic colors:  
-          color(255, 0, 0); // turn the RGB LED red 
+          setColor(255, 0, 0); // turn the RGB LED red 
+          
           delay(1000); // delay for 1 second  
-          color(0,255, 255); // turn the RGB LED green  
+          setColor(0,255, 255); // turn the RGB LED green  
           delay(1000); // delay for 1 second  
-          color(0, 0, 255); // turn the RGB LED blue  
+          setColor(0, 0, 255); // turn the RGB LED blue  
           delay(1000); // delay for 1 second 
           // Example blended colors:  
-          color(255,0,0); // turn the RGB LED red  
+          setColor(255,0,0); // turn the RGB LED red  
           delay(1000); // delay for 1 second  
-          color(237,109,0); // turn the RGB LED orange  
+          setColor(237,109,0); // turn the RGB LED orange  
           delay(1000); // delay for 1 second  
-          color(255,215,0); // turn the RGB LED yellow  
+          setColor(255,215,0); // turn the RGB LED yellow  
           delay(1000); // delay for 1 second  
-          color(34,139,34); // turn the RGB LED green  
+          setColor(34,139,34); // turn the RGB LED green  
           delay(1000); // delay for 1 second 
-          color(0,0,255); // turn the RGB LED blue  
+          setColor(0,0,255); // turn the RGB LED blue  
           delay(1000); // delay for 1 second
-          color(0,46,90); // turn the RGB LED  indigo 
+          setColor(0,46,90); // turn the RGB LED  indigo 
           delay(1000); // delay for 1 second
-          color(128,0,128); // turn the RGB LED purple  
+          setColor(128,0,128); // turn the RGB LED purple  
           delay(1000); // delay for 1 second
+          
+          */
     
   }
   else{
-   digitalWrite(redPin, HIGH); 
-      digitalWrite(greenPin, LOW);
-         digitalWrite(bluePin, LOW);
-         
+   digitalWrite(redPin, LOW); 
+   digitalWrite(greenPin, LOW);
+   digitalWrite(bluePin, LOW);      
   }
 }     
 /******************************************************/     
-void color (unsigned char red, unsigned char green, unsigned char blue)     // the color generating function  
+void changeColor(){
+  if(color == 1)
+    setColor(255, 0, 0); // turn the RGB LED red 
+  if(color == 2)
+          setColor(0,255, 255); // turn the RGB LED green  
+  if(color == 3)
+          setColor(0, 0, 255); // turn the RGB LED blue  
+  if(color == 4)
+  {
+          // Example blended colors:  
+          setColor(255,0,0); // turn the RGB LED red  
+  }
+  if(color == 5)
+          setColor(237,109,0); // turn the RGB LED orange  
+  if(color == 6)
+          setColor(255,215,0); // turn the RGB LED yellow  
+  if(color == 7)
+          setColor(34,139,34); // turn the RGB LED green  
+  if(color == 8)
+          setColor(0,0,255); // turn the RGB LED blue  
+  if(color == 9)
+          setColor(0,46,90); // turn the RGB LED  indigo 
+  if(color == 10)
+          setColor(128,0,128); // turn the RGB LED purple 
+          
+  color = color + 1;
+  if(color == 11)
+    color = 1;
+}
+void setColor (unsigned char red, unsigned char green, unsigned char blue)     // the color generating function  
 {    
           analogWrite(redPin, red);   
           analogWrite(bluePin, blue); 
